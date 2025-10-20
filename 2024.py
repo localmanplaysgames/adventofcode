0b52fcfdebb8fcb87daab76b00a3632405136e6d
localmanplaysgames <efitton5491@gmail.com>
2025-10-20 22:45:07 +0100

    d1p2 solved

--- 2024.py
+++ 2024.py
@@ -1,16 +1,30 @@
 import pandas as pd
 
-def day1(source_data):
+def load_data(source_data):
     df = pd.read_csv(source_data, sep=r'\s+', header=None, names=['a','b'], dtype=int)
     first_list, second_list = sorted(df['a'].tolist()), sorted(df['b'].tolist())
+    return first_list, second_list
+
+def day1_p1(source_data):
+    data = load_data(source_data)
     total_diff = 0
-    for i in range(len(first_list)):
-        total_diff += abs(first_list[i] - second_list[i])
+    for i in range(len(data[0])):
+        total_diff += abs(data[0][i] - data[1][i])
     return total_diff
+
+def day1_p2(source_data):
+    data = load_data(source_data)
+    similarity_score = 0
+    for i in range(len(data[0])):
+        matches = len([j for j in data[1] if j == data[0][i]])
+        if matches > 0:
+            similarity_score += data[0][i] * matches
+    return similarity_score
 
 def main():
     
-    print(f'Day 1 answer: {day1("data/2024_1.txt")}')
+    print(f'Day 1 part 1 answer: {day1_p1("data/2024_1.txt")}')
+    print(f'Day 1 part 2 answer: {day1_p2("data/2024_1.txt")}')
 
 if __name__ == "__main__":
     main()
