7bfec61ecfbde601e3c967ca574e5d6feb30dea7
localmanplaysgames <efitton5491@gmail.com>
2025-10-20 18:26:14 +0100

    restructure

--- 2024.py
+++ 2024.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
-def main():
-    
-    source_data = "data/2024_day1.txt"
-    df = pd.read_csv(source_data, sep='\s+', header=None, names=['a','b'], dtype=int)
+def day1(source_data):
+    df = pd.read_csv(source_data, sep=r'\s+', header=None, names=['a','b'], dtype=int)
     first_list, second_list = sorted(df['a'].tolist()), sorted(df['b'].tolist())
-
     total_diff = 0
-    print(f'List lengths = a: {len(first_list)}, b: {len(second_list)}')
     for i in range(len(first_list)):
         total_diff += abs(first_list[i] - second_list[i])
+    return total_diff
+
+def main():
     
-    print(total_diff)
+    print(f'Day 1 answer: {day1("data/2024_1.txt")}')
 
 if __name__ == "__main__":
     main()
